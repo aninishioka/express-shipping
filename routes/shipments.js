cf91bcc106721a9b53dfa9a15ab57c976e62d6e9
Anissa Nishioka <ani.nishioka@gmail.com>
2024-03-26 16:05:59 -0700

    json validation

--- routes/shipments.js
+++ routes/shipments.js
@@ -6,6 +6,9 @@ const router = new express.Router();
 
 const { shipProduct } = require("../shipItApi");
 
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/order.json")
+
 /** POST /ship
  *
  * VShips an order coming from json body:
@@ -15,11 +18,14 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
-  if (req.body === undefined) {
-    throw new BadRequestError();
+  const result = jsonschema.validate(req.body, orderSchema, {required: true});
+  console.log(result);
+  if (!result.valid) {
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
   }
-  const { productId, name, addr, zip } = req.body;
-  const shipId = await shipProduct({ productId, name, addr, zip });
+
+  const shipId = await shipProduct(req.body);
   return res.json({ shipped: shipId });
 });
 
